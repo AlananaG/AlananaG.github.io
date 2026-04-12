0b28ce7d8ce7052ff13c3cc463c93e8678e787ae
Alana <rgreen.alana@gmail.com>
2026-04-12 19:18:29 -0400

    Updated all pages & added images

--- itis3135/projects/menu.js
+++ itis3135/projects/menu.js
@@ -2,18 +2,40 @@ document.getElementById("cakes").innerHTML = `
   <h3>Cakes</h3>
 
   <article class="menu-item">
-    <img src="images/chocolate_cake.jpg" alt="Chocolate Cake">
+    <img src="cream_cheese_pound_cake.png" alt="Cream Cheese Pound Cake">
     <section>
-      <h4>Chocolate Cake - $25</h4>
-      <p>Rich chocolate cake with creamy frosting</p>
+      <h4>Cream Cheese Pound Cake - $25</h4>
+      <p>Very rich pound cake with creamy frosting</p>
     </section>
   </article>
 
   <article class="menu-item">
-    <img src="images/strawberry_cake.jpg" alt="Strawberry Cake">
+    <img src="s_crunch_cake.png" alt="Strawberry Crunch Cake">
     <section>
       <h4>Strawberry Cake - $28</h4>
       <p>Fresh strawberries with whipped icing</p>
+    </section>
+  </article>
+`;
+
+document.getElementById("cupcakes").innerHTML = `
+  <h3>Cupcakes</h3>
+    <article class="menu-item">
+    <img src="mg_cupcakes.png" alt="Mardi Gras Cupcakes">
+    <section>
+      <h4>Mardi Gras Cupcakes - $10</h4>
+      <p>Set of 10 cupcakes with whipped cream icing</p>
+    </section>
+  </article>
+`;
+
+document.getElementById("strawberries").innerHTML = `
+  <h3>Cookies & More</h3>
+    <article class="menu-item">
+    <img src="strawberries.png" alt="White Chocolate-Covered Strawberries">
+    <section>
+    <h4>White Chocolate-Covered Strawberries - $8</h4>
+    <p>Set of seven strawberries dipped and drizzled in white chocolate</p>
     </section>
   </article>
 `;
