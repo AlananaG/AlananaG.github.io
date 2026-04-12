ba17df41091e488a6618972bed67dffea24f5b19
Alana <rgreen.alana@gmail.com>
2026-04-11 21:43:14 -0400

    updated menu.js

---
+++ itis3135/projects/menu.js
@@ -0,0 +1,19 @@
+document.getElementById("cakes").innerHTML = `
+  <h3>Cakes</h3>
+
+  <article class="menu-item">
+    <img src="images/chocolate_cake.jpg" alt="Chocolate Cake">
+    <section>
+      <h4>Chocolate Cake - $25</h4>
+      <p>Rich chocolate cake with creamy frosting</p>
+    </section>
+  </article>
+
+  <article class="menu-item">
+    <img src="images/strawberry_cake.jpg" alt="Strawberry Cake">
+    <section>
+      <h4>Strawberry Cake - $28</h4>
+      <p>Fresh strawberries with whipped icing</p>
+    </section>
+  </article>
+`;
